3c633a2f2c0357f0fc375b57b5f0c141a0c3b268
Pushkinmazila <117484703+Pushkinmazila@users.noreply.github.com>
2023-03-22 20:53:24 +0300

    Create main_mango_sip_log_pass.ps1

---
+++ main_mango_sip_log_pass.ps1
@@ -0,0 +1,18 @@
+$login = "16916959" #Логин для входа в кабинет
+$password = "AHDlS03327" #Пароль для входа в кабинет
+$IURL = "https://lk.mango-office.ru/400183801/400254558/members/index" # Путь на страницу где список сотрудников
+
+
+$ie = New-Object -com "InternetExplorer.Application"
+$ie.visible = $true
+$ie.silent = $true
+$ie.Navigate($IURL)
+Wait-Event -Timeout 3
+$IE.Document.Forms | %{$_.item("login")} | % {$_.value = $login}
+$IE.Document.Forms | %{$_.item("password")} | % {$_.value = $password }
+Wait-Event -Timeout 3
+$IE.Document.Forms | %{$_.getElementsByClassName("prime")} | % {$_.Click()}
+Wait-Event -Timeout 3
+$a1 = (($ie.Document.IHTMLDocument3_getElementByID("b-members-data")).text | ConvertFrom-Json)
+$a1
+$IE.quit()
